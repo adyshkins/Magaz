40869c4dd4c2c1502eba9c163f8c0411d9abfff8
adyshkiss@outlook.com <adyshkiss@outlook.com>
2020-12-09 18:22:49 +0300

    Реализован поиск товаров по категориям. Внесли изменения в шаблон записей в таблице, добавили количество товаров. Выделение кнопки с категорией

--- Magaz/ActionWin.xaml.cs
+++ Magaz/ActionWin.xaml.cs
@@ -19,48 +19,97 @@ namespace Magaz
     /// </summary>
     public partial class ActionWin : Window
     {
-        Entities context = new Entities();
+        Entities context = new Entities(); // переместить
+
+        List<Product> productList = new List<Product>(); // список для фильтра
 
         public ActionWin()
         {
             InitializeComponent();
-            //ListProductGrid.ItemsSource = context.Product.ToList();
-            ListProduct.ItemsSource = context.Product.ToList();            
+
+            productList = context.Product.ToList();
+            ListProduct.ItemsSource = productList;
+        }
+
+        void BrushButton()
+        {
+            btnMainPage.Background = Brushes.LightGray;
+            btnAppliances.Background = Brushes.LightGray;
+            btnSmartphone.Background = Brushes.LightGray;
+            btnTV.Background = Brushes.LightGray;
+            btnPC.Background = Brushes.LightGray;
+            btnOffice.Background = Brushes.LightGray;
+            btnAccessories.Background = Brushes.LightGray;
+
+            btnMainPage.Style = null;
         }
 
         private void btnMainPage_Click(object sender, RoutedEventArgs e)
         {
-            ListProduct.ItemsSource = context.Product.ToList();
+            BrushButton();
+            productList = context.Product.ToList();
+            ListProduct.ItemsSource = productList;
+            btnMainPage.Background = Brushes.Red;
         }
 
         private void btnAppliances_Click(object sender, RoutedEventArgs e)
         {
-            ListProduct.ItemsSource = context.Product.Where(i => i.IdProductCategory == 1).ToList();
+            BrushButton();
+            productList = context.Product.Where(i => i.IdProductCategory == 1).ToList();
+            ListProduct.ItemsSource = productList;
+            btnAppliances.Background = Brushes.Red;
+
         }
 
         private void btnSmartphone_Click(object sender, RoutedEventArgs e)
         {
-            ListProduct.ItemsSource = context.Product.Where(i => i.IdProductCategory == 2).ToList();
+            BrushButton();
+            productList = context.Product.Where(i => i.IdProductCategory == 2).ToList();
+            ListProduct.ItemsSource = productList;
+            btnSmartphone.Background = Brushes.Red;
+
         }
 
         private void btnTV_Click(object sender, RoutedEventArgs e)
         {
-            ListProduct.ItemsSource = context.Product.Where(i => i.IdProductCategory == 3).ToList();
+            BrushButton();
+            productList = context.Product.Where(i => i.IdProductCategory == 3).ToList();
+            ListProduct.ItemsSource = productList;
+            btnTV.Background = Brushes.Red;
+
         }
 
         private void btnPC_Click(object sender, RoutedEventArgs e)
         {
-            ListProduct.ItemsSource = context.Product.Where(i => i.IdProductCategory == 4).ToList();
+            BrushButton();
+            productList = context.Product.Where(i => i.IdProductCategory == 4).ToList();
+            ListProduct.ItemsSource = productList;
+            btnPC.Background = Brushes.Red;
+
         }
 
         private void btnOffice_Click(object sender, RoutedEventArgs e)
         {
-            ListProduct.ItemsSource = context.Product.Where(i => i.IdProductCategory == 5).ToList();
+            BrushButton();
+            productList = context.Product.Where(i => i.IdProductCategory == 5).ToList();
+            ListProduct.ItemsSource = productList;
+            btnOffice.Background = Brushes.Red;
+
         }
 
         private void btnAccessories_Click(object sender, RoutedEventArgs e)
         {
-            ListProduct.ItemsSource = context.Product.Where(i => i.IdProductCategory == 6).ToList();
+            BrushButton();
+            productList = context.Product.Where(i => i.IdProductCategory == 6).ToList();
+            ListProduct.ItemsSource = productList;
+            btnAccessories.Background = Brushes.Red;
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e) // поиск товаров по имени и описанию
+        {
+            ListProduct.ItemsSource = productList
+                .Where(i => i.ProductName.Contains(txtSearch.Text))
+                .Where(i => i.ProductDescription.Contains(txtSearch.Text));
         }
     }
 }
